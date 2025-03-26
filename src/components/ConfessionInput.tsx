89f0c28cef73e4d96b54054826c5a738d46be5b0
Jobin Babu Ayathil <jobinb6444@outlook.com>
2025-03-26 19:40:41 +0530

    Update ConfessionInput.tsx

--- src/components/ConfessionInput.tsx
+++ src/components/ConfessionInput.tsx
@@ -65,7 +65,7 @@ const ConfessionInput: React.FC = () => {
               className="w-full bg-transparent border-none outline-none text-white py-3 px-6 text-base sm:text-lg"
               maxLength={maxLength}
               disabled={loading}
-              style={{ paddingRight: "70px" }}
+              style={{ paddingRight: "50px" }}
             />
             
             {/* Character counter - made more dim */}
